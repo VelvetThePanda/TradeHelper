02cdcb82b988446f728c6c8d29aad6aa90fffb76
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-05-14 13:06:13 -0400

    did I forget to commit this?

--- TradeHelper/Program.cs
+++ TradeHelper/Program.cs
@@ -4,6 +4,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Remora.Discord.Commands.Extensions;
+using Remora.Discord.Commands.Services;
 using Remora.Discord.Hosting.Extensions;
 using TradeHelper.Data;
 using TradeHelper.Data.MediatR;
@@ -17,11 +19,16 @@ var host = Host.CreateDefaultBuilder()
     })
     .ConfigureServices((context, services) =>
     {
-        services.AddDbContext<TradeContext>();
-        services.AddMediatR(typeof(TradeContext).Assembly);
-        
+        services
+            .AddMediatR(typeof(TradeContext))
+            .AddDbContext<TradeContext>()
+            .AddDiscordCommands(true);
     })
     .AddDiscordService(s => s.GetRequiredService<IConfiguration>().GetConnectionString("Discord"))
     .UseConsoleLifetime()
     .Build();
     
+host.Services.GetRequiredService<TradeContext>().Database.MigrateAsync();
+host.Services.GetRequiredService<SlashService>().UpdateSlashCommandsAsync();
+
+await host.RunAsync();
