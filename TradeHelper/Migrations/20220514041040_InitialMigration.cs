011ae4f54b6ce57d6e7ec9c7e18bc8a132658b83
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-05-14 11:54:14 -0400

    Migrations :L

---
+++ TradeHelper/Migrations/20220514041040_InitialMigration.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TradeHelper.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ID = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    Reputation = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trades",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "TEXT", nullable: false),
+                    GuildID = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    OwnerID = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    ClaimerID = table.Column<ulong>(type: "INTEGER", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ClaimedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    CompletedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    ExpiresAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Offering = table.Column<string>(type: "TEXT", nullable: false),
+                    Requesting = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trades", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Trades_Users_ClaimerID",
+                        column: x => x.ClaimerID,
+                        principalTable: "Users",
+                        principalColumn: "ID");
+                    table.ForeignKey(
+                        name: "FK_Trades_Users_OwnerID",
+                        column: x => x.OwnerID,
+                        principalTable: "Users",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trades_ClaimerID",
+                table: "Trades",
+                column: "ClaimerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trades_OwnerID",
+                table: "Trades",
+                column: "OwnerID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Trades");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
