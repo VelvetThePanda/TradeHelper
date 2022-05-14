ebb8111e615f9ffd02c65528e501b936bcfde83d
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-05-14 11:52:18 -0400

    Begin work on bot stuff

--- TradeHelper/Program.cs
+++ TradeHelper/Program.cs
@@ -1,15 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Remora.Discord.Hosting.Extensions;
 using TradeHelper.Data;
 using TradeHelper.Data.MediatR;
 
-var services = new ServiceCollection()
-    .AddDbContext<TradeContext>()
-    .AddMediatR(typeof(TradeContext))
-    .BuildServiceProvider();
+var host = Host.CreateDefaultBuilder()
+    .ConfigureAppConfiguration(configuration =>
+    {
+        configuration.SetBasePath(Directory.GetCurrentDirectory());
+        configuration.AddJsonFile("appSettings.json", true, false);
+        configuration.AddUserSecrets("VelvetThePanda-TradeHelper", false);
+    })
+    .ConfigureServices((context, services) =>
+    {
+        services.AddDbContext<TradeContext>();
+        services.AddMediatR(typeof(TradeContext).Assembly);
+        
+    })
+    .AddDiscordService(s => s.GetRequiredService<IConfiguration>().GetConnectionString("Discord"))
+    .UseConsoleLifetime()
+    .Build();
     
-var mediator = services.GetService<IMediator>();
-
-await mediator.Send(new UserExists.Request(default));
